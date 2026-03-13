acddd237ff959e07539fcf6469bda378576e7603
Tropiscape <tropiscapeaquatics@gmail.com>
2026-03-13 13:06:56 -0600

    Update Auto_Fert_Doser.ino
    Fixed PUMP_DURATION inaccuracy.

--- Auto_Fert_Doser.ino
+++ Auto_Fert_Doser.ino
@@ -51,7 +51,7 @@ constexpr uint8_t BUTTON_PIN = 2;  // Pin connected to the manual dose button
 constexpr unsigned long DEBOUNCE_DELAY = 50;  // Delay for button debounce (in milliseconds)
 constexpr unsigned long DISPLAY_UPDATE_INTERVAL = 1000;  // Interval for updating the display (in milliseconds)
 constexpr float PUMP_CALIBRATION = 1.04;  // Pump calibration factor (for 1 mL/sec)
-constexpr int PUMP_DURATION = static_cast<int>(PUMP_CALIBRATION * (doseAmount * 1000));
+constexpr int PUMP_DURATION = (((PUMP_CALIBRATION - 1.0) * -1.0) + 0.9) * (doseAmount * 1000); // The time (in milliseconds) for the pump to run.
 constexpr int MAX_PUMP_DURATION = PUMP_DURATION + 1; // Maximum pump runtime to prevent any hangups
 const uint8_t numDoses = sizeof(doseSchedule) / sizeof(doseSchedule[0]);  // Number of doses in the schedule
 
